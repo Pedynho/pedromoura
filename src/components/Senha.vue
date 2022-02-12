cb1171e73e215557d5c4926e6ad37f16b7bdb1c6
pedynho <pedynho@DESKTOP-RMO0D4P.localdomain>
2022-02-12 14:41:04 -0300

    feat: implementação do componehnte de validação de senha

--- src/components/Senha.vue
+++ src/components/Senha.vue
@@ -8,25 +8,34 @@
         de texto.
     </div>
 
-    <div class="form-group">
-        <label for="senha"> <span> Senha: </span> </label>
-        <input v-model="senha" type="password" id="senha" placeholder="Insira sua senha..." required/>
-
-        <label for="senha"> <span> Repita a senha: </span> </label>
-        <input v-model="senhaConf" type="password" id="senhaConf" placeholder="Repita a senha..." required/>
+    <div class="form-container">
+        <div class="form-card">
+            <div class="form-body">
+                <div class="form-group">
+                    <div class="input">
+                        <input v-on:keyup="validation" v-model="senha" type="password" id="senha" placeholder="Insira sua senha..." required/>
+                    </div>
+                    <div class="input">
+                        <input v-on:keyup="validation" v-model="senhaConf" type="password" id="senhaConf" placeholder="Repita a senha..." required/>
+                    </div>
+                </div>
+                <div class="senha-err">
+                    <ul>
+                        <li :class="{'conferido': passValidation.lowcase}">Pelo menos um caractere minúsculo</li>
+                        <li :class="{'conferido': passValidation.uppercase}">Pelo menos um caractere maiúsculo</li>
+                        <li :class="{'conferido': passValidation.special}">Pelo menos um caractere especial</li>
+                        <li :class="{'conferido': passValidation.number}">Pelo menos um caractere numérico</li>
+                        <li :class="{'conferido': passValidation.eigthCh}">Pelo menos oito caracteres</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="form-footer">
+                <span v-if="!passValidation.equals" class="senha-err">As duas senhas devem ser iguais!</span>
+            </div>
+        </div>
     </div>
 
-    <span v-if="placeholder" class="senha-err">As duas senhas devem ser iguais!</span>
 
-    <span v-if="placeholder" class="senha-err">
-        <ul>
-            <li :class="{'conferido': placeholder}">Pelo menos um caractere minúsculo</li>
-            <li :class="{'conferido': placeholder}">Pelo menos um caractere maiúsculo</li>
-            <li :class="{'conferido': placeholder}">Pelo menos um caractere especial</li>
-            <li :class="{'conferido': placeholder}">Pelo menos um caractere numérico</li>
-            <li :class="{'conferido': placeholder}">Pelo menos oito caracteres</li>
-        </ul>
-    </span>
 </template>
 
 <script>
@@ -35,57 +44,71 @@
         data() {
             return {
                 senha: "",
-                senhaConf: ""
+                senhaConf: "",
+                passValidation: {
+                    equals: true,
+                    lowcase: false,
+                    uppercase: false,
+                    special: false,
+                    number: false,
+                    eigthCh: false,
+                }
             };
         },
-        computed: {
-            placeholder() {
-                return true;
-            }
+        methods: {
+            validation() { 
+                this.passValidation.eigthCh = this.senha.length >= 8
+                this.passValidation.lowcase = this.senha.match(/[a-z]/g)            
+                this.passValidation.uppercase = this.senha.match(/[A-Z]/g)
+                this.passValidation.special = this.senha.match(/[^A-Za-z0-9]/g)           
+                this.passValidation.number = this.senha.match(/[0-9]/g)
+                this.passValidation.equals = this.senha === this.senhaConf          
+            },
         },
-        methods: {}
     }
 </script>
 
 <style scoped>
- .form-group {
-     display: grid;
-     margin: 0 auto;
-     grid-template-columns: auto 1fr;
-     grid-template-rows: auto;
-     grid-column-gap: 20px;
-     grid-row-gap: 5px;
-     width: 80%;
-     margin-bottom: 5px;
- }
+    .form-container {
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        padding: 2em;
+    }
+    .form-card {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        border: 2px solid #ccc;
+        box-shadow: 0 0 4px 3px rgba(0,0,0,0.12);
+        padding: 10px;
+    }
+    .form-body{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        padding: 1em;
+    }
+    .input {
+        padding: 5px;
+    }
+    .input input{
+        font-size: 1em;
+        outline: none;
+        border: 0;
+        height: 1.5em;
+        border-bottom: 2px solid #ddb6f2;
+    }
+    .conferido {
+        color: green !important;
+    }
 
- @media query screen and (max-width: 800px) {
-     .form-group {
-         width: calc(100% - 20px);
-     }
- }
-
- .form-group > label {
-     display: flex;
-     justify-content: center;
-     align-content: center;
-     flex-direction: column;
- }
-
- .form-group > label > span {
-     width: 100%;
- }
-
- .form-group > input {
-     border: 1px solid grey;
-     border-radius: 5px;
- }
-
- .conferido {
-     color: green !important;
- }
-
- .senha-err {
-     color: red;
- }
+    .senha-err {
+        font-size: 0.8em;
+        color: red;
+    }
 </style>
